e8d4ec0301966e5254357996a77e890d0316f4c6
Manoel Amaro <mano.lira@gmail.com>
2016-04-17 10:56:09 -0300

    Adds comments to the controller and job

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,15 @@
-require 'sidekiq/api'
-
 class HomeController < ApplicationController
 
   def index
   end
 
+  ##
+  # Method to perform the search.
+  # It also saves the query into Redis and creates a scheduled Job
+  # to consume this query. If the user keeps typing, the query is updated
+  # and the date too, so the scheduled job will only run after 3 sec since
+  # the last time the user typed something.
+  ##
   def search
     $redis.set "#{session.id}.query", params[:q]
     $redis.set "#{session.id}.last_update", Time.now.to_i
--- app/workers/save_query.rb
+++ app/workers/save_query.rb
@@ -2,6 +2,12 @@ class SaveQuery
   include Sidekiq::Worker
   sidekiq_options queue: 'priority', retry: false
 
+  ##
+  # Job that will count the queries perfomed by users. It checks if
+  # the last_update data is more than 3 seconds from now. If so, then
+  # the user stopped typing for 3 sec, and the query is saved into Redis,
+  # counting it. Else, the job is rescheduled to 3 sec from now.
+  ##
   def perform(session_id)
     timestamp = $redis.get("#{session_id}.last_update").to_i
     if timestamp + 3.seconds < Time.now.to_i
