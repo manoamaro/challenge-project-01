f8985e59c46dd2c8c7f67886221e41304ab4fae6
Manoel Amaro <mano.lira@gmail.com>
2016-04-19 07:28:46 -0300

    Fixes controller spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,11 +2,15 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
+  before(:all) do
+    $redis = MockRedis.new
+  end
+
   describe "GET #search" do
     it "returns http success" do
       get :search
       expect(response).to have_http_status(:success)
-      response.body.should be_blank
+      expect(response.body).to be_blank
     end
   end
 
